c134f89fe55a711af8e143297ea06907da45e4b9
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-02 00:11:47 +0530

    Deleted Test Files and Added the Quote component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import './App.css';
 import Home from './components/home';
+
 
 function App() {
   return (
--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 function Quote() {
     return (
-      <h5>quote</h5>
+      <div className="quote-container">
+        <img className="quote-open" src="https://drive.google.com/uc?export=download&id=1ao5VQdtoX3kJ7X9Q5owM1Jhn10mdpy5p" alt="quote-open"></img>
+        <div className="quote-text">
+            <p className="quote">
+                Plants are the unsung heroes of the natural world, providing us with oxygen, beauty, and sustenance. Let's celebrate and care for them.
+            </p>
+            <p className="credit">
+                -Md Imran Raj
+            </p>
+        </div>
+        <img className="quote-close" src="https://drive.google.com/uc?export=download&id=1flEyndEzPs--MFFCFGq94HdueUzrR5fa" alt="quote-close"></img>
+      </div>
     );
 }
 
