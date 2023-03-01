badb91380892b0e3cb20cc3154132fe531723240
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-02 01:14:30 +0530

    added the navbar and centered the Quote

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,49 @@ import React from 'react';
 function Navbar() {
 
     return (
-      <h5>navbar</h5>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+          <img className= "logo-img" src="https://drive.google.com/uc?export=download&id=1i8iQzxti4HNAPGGx1PxvINzAhvNJA6br" alt="logo"/>
+            </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav navbar-right-items">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Plant Room
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item" href="#">Category 1</a></li>
+                  <li><a className="dropdown-item" href="#">Category 2</a></li>
+                  <li><a className="dropdown-item" href="#">Category 3</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Blogs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">My Plants</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#"><img class="profile-blank-img" src="https://drive.google.com/uc?export=download&id=1LlZ-cNQVrnfUTFwEg5lCVuFw4OPbd7wg" alt="" srcset=""></img></a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
 }
 
