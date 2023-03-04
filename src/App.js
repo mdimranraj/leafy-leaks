a8b216350825804fcc76da13e98de0d35dc1d41b
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-05 01:28:44 +0530

    conflict changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home/>
         
+          <Router>
+          <Navbar />
+          <Routes>
+              <Route exact path='/' exact element={<Home />} />
+              <Route path='/plantroom' element={<PlantRoom/>} />
+              <Route path='/blogs' element={<Blogs/>} />
+              <Route path='/aboutus' element={<AboutUs/>} />
+              <Route path='/contact' element={<Contact/>} />
+              <Route path='/myplants' element={<MyPlants/>} />
+          </Routes>
+          <Footer />
+          </Router>
       </header>
     </div>
   );
