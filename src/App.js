909d7e9647cbd45883de0e3e1fa6d6fcc3fdbf03
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-05 01:26:34 +0530

    Making Routing

--- src/App.js
+++ src/App.js
@@ -9,11 +9,23 @@ function App() {
     <div className="App">
       
       <header className="App-header">
-        <Home/>
         
+          <Router>
+          <Navbar />
+          <Routes>
+              <Route exact path='/' exact element={<Home />} />
+              <Route path='/plantroom' element={<PlantRoom/>} />
+              <Route path='/blogs' element={<Blogs/>} />
+              <Route path='/aboutus' element={<AboutUs/>} />
+              <Route path='/contact' element={<Contact/>} />
+              <Route path='/myplants' element={<MyPlants/>} />
+          </Routes>
+          <Footer />
+          </Router>
       </header>
     </div>
   );
 }
+import Footer from './components/footer';
 
 export default App;
