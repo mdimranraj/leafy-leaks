e3669094e70978b24ad47e44c641f42ae8672504
Ankush <ankushkumar747474@gmail.com>
2023-03-04 20:53:25 +0530

    logo corrected

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-          <img className= "logo-img" src="https://drive.google.com/uc?export=download&id=1i8iQzxti4HNAPGGx1PxvINzAhvNJA6br" alt="logo"/>
+          <img className= "logo-img" src="https://drive.google.com/uc?export=download&id=11l-zqKjyyVmgYa5f6S8iqijAkhpAvu97" alt="logo"/>
             </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
