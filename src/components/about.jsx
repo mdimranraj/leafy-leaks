b7f3db8f935f416c6712013a9bcd972b1ce8f93b
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-06 14:57:01 +0530

    corrected the aboutus page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,12 +14,14 @@ function About () {
             <h2>The Green Team</h2>
             <div className='team-container-flex'>
                 <div className='team-member'>
+                    <div className="team-member-left-flex">
                     <img className="team-member-img" src='https://drive.google.com/uc?export=download&id=1IM0jpjIn2URghvhdoK1kCsuyXvGdChYq' alt='member'></img>
                     <div className='member-details'>
                         <h4>Md Imran Raj</h4>
                         <h6>( Full Stack Web and Android Developer, UI/UX Developer )</h6>
                         <h5>Role in team : <span className='role'>Lead Developer, Frontend and Backend Manager </span></h5>
                     </div>
+                    </div>
                     <div className='social-details'>
                         <h5>Follow me on :</h5>
                         <div>
@@ -32,12 +34,14 @@ function About () {
 
                 </div>
                 <div className='team-member'>
+                    <div className="team-member-left-flex">
                     <img className="team-member-img" src='https://media.licdn.com/dms/image/D4D03AQEZQZKjRDnd2A/profile-displayphoto-shrink_400_400/0/1678021703394?e=1683763200&v=beta&t=EDVaAZh5synLL-WejukSkO2pBXPj8azRdV9yK5gdUcg' alt='member'></img>
                     <div className='member-details'>
                         <h4>Ankush Kumar</h4>
                         <h6>( Data Enthusiast, Frontend Developer )</h6>
                         <h5>Role in team : <span className='role'>Info Manager and Frontend Advisor </span></h5>
                     </div>
+                    </div>
                     <div className='social-details'>
                         <h5>Follow me on :</h5>
                         <div>
@@ -49,11 +53,13 @@ function About () {
                     </div>
                 </div>
                 <div className='team-member'>
+                    <div className="team-member-left-flex">
                     <img className="team-member-img" src='https://media.licdn.com/dms/image/D4D03AQHmP12OP4gbLg/profile-displayphoto-shrink_400_400/0/1675711088202?e=1683763200&v=beta&t=Fs2xVAFeiUaW3_lvsjjaedAlJtw9qWrlw7QX9TPPOhI' alt='member'></img>
                     <div className='member-details'>
                         <h4>Jigyanshu Raj</h4>
                         <h6>( UI/UX Designer and Researcher, Data Enthusiast )</h6>
                         <h5>Role in team : <span className='role'>UI Lead </span></h5>
+                    </div>
                     </div>
                     <div className='social-details'>
                         <h5>Follow me on :</h5>
