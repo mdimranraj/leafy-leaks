bbbd827289e81d7f12154a13b59655474ced09c8
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-06 15:14:32 +0530

    links corrected in aboutus

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -26,7 +26,7 @@ function About () {
                         <h5>Follow me on :</h5>
                         <div>
                         <a href="https://www.linkedin.com/in/md-imran-raj-952659201/" target='_blank'><img className='linkedIn-img' src='https://drive.google.com/uc?export=download&id=1fW7AugDk_OyfBbZYpes7wnhbLM-5Xdr5'></img></a>
-                        <a href='' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
+                        <a href='https://github.com/mdimranraj' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
                         <a href='mailto:mdimranraj786@gmail.com' target='_blank'><img src='https://drive.google.com/uc?export=download&id=1oiAp5iqS67x17KAnaDO27uy8AINmu0z1' /></a>
 
                         </div>
@@ -45,9 +45,9 @@ function About () {
                     <div className='social-details'>
                         <h5>Follow me on :</h5>
                         <div>
-                        <a href="https://www.linkedin.com/in/md-imran-raj-952659201/" target='_blank'><img className='linkedIn-img' src='https://drive.google.com/uc?export=download&id=1fW7AugDk_OyfBbZYpes7wnhbLM-5Xdr5'></img></a>
-                        <a href='' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
-                        <a href='mailto:mdimranraj786@gmail.com' target='_blank'><img src='https://drive.google.com/uc?export=download&id=1oiAp5iqS67x17KAnaDO27uy8AINmu0z1' /></a>
+                        <a href="https://www.linkedin.com/in/ankush-kumar-b76335189/" target='_blank'><img className='linkedIn-img' src='https://drive.google.com/uc?export=download&id=1fW7AugDk_OyfBbZYpes7wnhbLM-5Xdr5'></img></a>
+                        <a href='https://github.com/ankushkumar74' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
+                        <a href='mailto:' target='_blank'><img src='https://drive.google.com/uc?export=download&id=1oiAp5iqS67x17KAnaDO27uy8AINmu0z1' /></a>
 
                         </div>
                     </div>
@@ -64,9 +64,9 @@ function About () {
                     <div className='social-details'>
                         <h5>Follow me on :</h5>
                         <div>
-                        <a href="https://www.linkedin.com/in/md-imran-raj-952659201/" target='_blank'><img className='linkedIn-img' src='https://drive.google.com/uc?export=download&id=1fW7AugDk_OyfBbZYpes7wnhbLM-5Xdr5'></img></a>
-                        <a href='' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
-                        <a href='mailto:mdimranraj786@gmail.com' target='_blank'><img src='https://drive.google.com/uc?export=download&id=1oiAp5iqS67x17KAnaDO27uy8AINmu0z1' /></a>
+                        <a href="https://www.linkedin.com/in/hashjiggy/" target='_blank'><img className='linkedIn-img' src='https://drive.google.com/uc?export=download&id=1fW7AugDk_OyfBbZYpes7wnhbLM-5Xdr5'></img></a>
+                        <a href='https://github.com/hashjiggy' target='_blank'><img src='https://drive.google.com/uc?export=download&id=16shR8sum3xnPdJchaag8k5229tvUj5qn' /></a>
+                        <a href='mailto:jigyanshu.019@gmail.com' target='_blank'><img src='https://drive.google.com/uc?export=download&id=1oiAp5iqS67x17KAnaDO27uy8AINmu0z1' /></a>
 
                         </div>
                     </div>
