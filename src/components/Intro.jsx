757e404b81d9e9708bcc4de32b1bd8e6835c8347
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-12 20:14:02 +0530

    details adjusted

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,12 +4,10 @@ import Carousel from './Carousel';
 function Intro(){
     return (
         <div class="main-page-container">
-        <div class="main-page-first">
             <div class="main-page-first-text">
                 <h2 class="main-page-first-heading"> The world of Plants </h2>
                 <p class="main-page-first-para">Get rooted in the world of plants with LeafyLeaks - the ultimate encyclopedia for all things leafy and lovely!</p>
             </div>
-        </div>
         </div>
     );
 }
--- src/components/plantdetails.jsx
+++ src/components/plantdetails.jsx
@@ -27,6 +27,30 @@ function PlantDetail(){
                     </table>
                 </div>
             </div>
+            <div className='plant-detail-box-2'>
+                <div className='box-2-part1'>
+                    <h3>Description </h3>
+                    <div>
+                    A specific kind of flowering shrub is the rose. The Latin word Rosa is the source of its name. All roses were originally wild: they grew across North America, Europe, northwest Africa and many regions of Asia and Oceania. There are about 100 different species of roses. Although wild rose species can be produced in gardens, most garden roses are cultivars that have been selected by humans. The stems of most roses have spines. This is a typical plant defensive system.
+ Roses can be found in abundance in gardens. They can also be found in vineyards. A rose bush is put at the end of each row of grapes in a large vineyard. The vine growers can tell that their vines are healthy as long as the roses are healthy. 
+
+                    </div>
+                </div>
+                <div className='box-2-part2'>
+                    <h3>Uses and Benefits</h3>
+                    <div>
+                    Roses have a pleasant smell. The aroma of the rose is produced by microscopic perfume glands on the petals, which may be seen under a strong microscope. Rose petals are sometimes dried and wrapped so that they can be used for decoration or perfume.
+Roses can be found in abundance in gardens. They can also be found in vineyards. A rose bush is put at the end of each row of grapes in a large vineyard. The vine growers can tell that their vines are healthy as long as the roses are healthy.
+
+                    </div>
+                </div>
+                <div className='box-2-part3'>
+                    <h3>How to Grow</h3>
+                    <div>
+                    Put the rose plant in the sun for 3 to 4 hours every morning so that it can grow at the proper period. Provide water to the plant as needed to ensure that there are no problems. This floral plant makes little use of chemical fertilisers or fertilisers.
+                    </div>
+                </div>
+            </div>
         </div>
 
         </>
