1a511cfa529db053d3deb071a0d4ac578ed2cf04
Md Imran Raj <mdimranraj786@gmail.com>
2023-03-28 16:05:12 +0530

    register/login complete

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+       
+       <form className='auth-form'> 
+       <h3>
+           Login to your Leaf
+        </h3>
+
+        <div className="each-form mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
+        </div>
+        <div className="each-form mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="passwordInput" />
+        </div>
+        
+        <button type="submit" className="btn btn-success">Log In</button>
+        <div>Not Registered...?</div>
+       </form>
+
+    </>
   )
 }
 
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,20 +3,30 @@ import React from 'react'
 const Register = () => {
   return (
     <>
-        <h3>
-            Register with us
-        </h3>
+       
        <form className='auth-form'> 
+       <h3>
+           Join us. Be a Leaf.
+        </h3>
+        <div className="each-form mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Name</label>
+            <input type="text" className="form-control" id="nameInput" />
+        </div>
         <div className="each-form mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" />
         </div>
         <div className="each-form mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" className="form-control" id="passwordInput" placeholder='For ex : abdj@123CD'/>
+        </div>
+        <div className="each-form mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
+            <input type="password" className="form-control" id="passwordInput" placeholder='mention same password as above'/>
         </div>
         
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-success">Register</button>
+        
        </form>
 
     </>
